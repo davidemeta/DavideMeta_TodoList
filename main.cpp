53a7afa2cb3e151e7d9ac1e31337796309b27229
davidemeta <metadavide95@gmail.com>
2024-08-26 18:33:16 +0200

    Creation of the class Attivita

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
-#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+class Attivita {
+public:
+    Attivita(const std::string &descrizione, bool completata = false)
+        : descrizione(descrizione), completata(completata) {
+    }
+
+    std::string getDescrizione() const {
+        return descrizione;
+    }
+
+    bool isCompletata() const {
+        return completata;
+    }
+
+    void setCompletata(bool completata) {
+        this->completata = completata;
+    }
+
+    std::string toString() const {
+        std::stringstream ss;
+        ss << descrizione << "," << completata;
+        return ss.str();
+    }
+
+    static Attivita fromString(const std::string &str) {
+        std::stringstream ss(str);
+        std::string descrizione;
+        std::string completataStr;
+        std::getline(ss, descrizione, ',');
+        std::getline(ss, completataStr);
+        bool completata = (completataStr == "1");
+        return Attivita(descrizione, completata);
+    }
+
+private:
+    std::string descrizione;
+    bool completata;
+};
+
+int main() {
 }
