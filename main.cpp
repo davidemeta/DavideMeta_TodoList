a0c6e57b5932d8b4d237617519e71370cb3c290a
davidemeta <metadavide95@gmail.com>
2024-08-26 18:37:24 +0200

    Creation of the class TodoList

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include <fstream>
+#include <vector>
 #include <string>
 #include <sstream>
 
@@ -39,6 +41,52 @@ public:
 private:
     std::string descrizione;
     bool completata;
+};
+
+class TodoList {
+public:
+    void aggiungiAttivita(const Attivita &attivita) {
+        attivitaList.push_back(attivita);
+    }
+
+    void rimuoviAttivita(int index) {
+        if (index >= 0 && index < attivitaList.size()) {
+            attivitaList.erase(attivitaList.begin() + index);
+        }
+    }
+
+    std::vector<Attivita> getAttivita() const {
+        return attivitaList;
+    }
+
+    void salvaSuDisco(const std::string &filename) const {
+        std::ofstream file(filename);
+        if (file.is_open()) {
+            for (const auto &attivita: attivitaList) {
+                file << attivita.toString() << std::endl;
+            }
+            file.close();
+        } else {
+            std::cerr << "Impossibile aprire il file per la scrittura." << std::endl;
+        }
+    }
+
+    void caricaDaDisco(const std::string &filename) {
+        std::ifstream file(filename);
+        if (file.is_open()) {
+            attivitaList.clear();
+            std::string line;
+            while (std::getline(file, line)) {
+                attivitaList.push_back(Attivita::fromString(line));
+            }
+            file.close();
+        } else {
+            std::cerr << "Impossibile aprire il file per la lettura." << std::endl;
+        }
+    }
+
+private:
+    std::vector<Attivita> attivitaList;
 };
 
 int main() {
