99ad00554ca37ebe251454fa60e76479504cf4c8
davidemeta <metadavide95@gmail.com>
2024-08-29 12:49:43 +0200

    The unit testing of runAllTest.cpp now is completed

--- test/runAllTest.cpp
+++ test/runAllTest.cpp
@@ -1,3 +1,9 @@
 //
 // Created by Davide Meta on 28/08/24.
 //
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
