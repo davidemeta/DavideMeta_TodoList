27f14289a91f6883db7bae942f1b28b19579fac4
davidemeta <metadavide95@gmail.com>
2024-09-06 09:59:02 +0200

    Added two new methods, OnCheck() for the check-box and OnSearch() for the search bar

--- MyFrame.h
+++ MyFrame.h
@@ -4,7 +4,9 @@
 
 #ifndef MYFRAME_H
 #define MYFRAME_H
+
 #include <wx/wx.h>
+#include <wx/checklst.h>
 #include "TodoList.h"
 
 class MyFrame : public wxFrame {
@@ -13,17 +15,24 @@ public:
 
 private:
     void OnAdd(wxCommandEvent &event);
-
     void OnRemove(wxCommandEvent &event);
-
     void OnSave(wxCommandEvent &event);
-
     void OnLoad(wxCommandEvent &event);
-
+    void OnCheck(wxCommandEvent &event);   // New method to handle check box
+    void OnSearch(wxCommandEvent &event);  // New method to handle search
     void UpdateListBox();
 
-    wxListBox *listBox;
+    wxCheckListBox *listBox;
+    wxTextCtrl *searchCtrl;  // Member variable for the search control
+    wxStaticText *remainingText;
     TodoList todoList;
+
+    enum {
+        ID_Add = 1,
+        ID_Remove,
+        ID_Save,
+        ID_Load
+    };
 };
 
-#endif //MYFRAME_H
+#endif // MYFRAME_H
