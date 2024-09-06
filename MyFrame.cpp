93581da1ecdc5c793b88337f262e41bd13a9b0e3
davidemeta <metadavide95@gmail.com>
2024-09-06 10:00:43 +0200

    Added the implementation of the two methods in MyFrame.cpp

--- MyFrame.cpp
+++ MyFrame.cpp
@@ -5,12 +5,21 @@
 #include "MyFrame.h"
 
 MyFrame::MyFrame(const wxString &title)
-    : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(450, 340)) {
+    : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(510, 540)) {
     wxPanel *panel = new wxPanel(this, wxID_ANY);
 
     wxBoxSizer *vbox = new wxBoxSizer(wxVERTICAL);
 
-    listBox = new wxListBox(panel, wxID_ANY);
+    // Add a search bar
+    wxBoxSizer *hboxSearch = new wxBoxSizer(wxHORIZONTAL);
+    searchCtrl = new wxTextCtrl(panel, wxID_ANY); // Initialize the search control
+    wxButton *searchButton = new wxButton(panel, wxID_ANY, wxT("Search"));
+    hboxSearch->Add(searchCtrl, 1, wxEXPAND | wxALL, 5);
+    hboxSearch->Add(searchButton, 0, wxEXPAND | wxALL, 5);
+
+    vbox->Add(hboxSearch, 0, wxEXPAND | wxALL, 10);
+
+    listBox = new wxCheckListBox(panel, wxID_ANY);
     vbox->Add(listBox, 1, wxEXPAND | wxALL, 10);
 
     wxBoxSizer *hbox1 = new wxBoxSizer(wxHORIZONTAL);
@@ -29,18 +38,21 @@ MyFrame::MyFrame(const wxString &title)
 
     vbox->Add(hbox2, 0, wxALIGN_CENTER | wxTOP | wxBOTTOM, 10);
 
+    remainingText = new wxStaticText(panel, wxID_ANY, "");
+    vbox->Add(remainingText, 0, wxALIGN_CENTER | wxTOP | wxBOTTOM, 10);
+
     panel->SetSizer(vbox);
 
-    Connect(ID_Add, wxEVT_COMMAND_BUTTON_CLICKED,
-            wxCommandEventHandler(MyFrame::OnAdd));
-    Connect(ID_Remove, wxEVT_COMMAND_BUTTON_CLICKED,
-            wxCommandEventHandler(MyFrame::OnRemove));
-    Connect(ID_Save, wxEVT_COMMAND_BUTTON_CLICKED,
-            wxCommandEventHandler(MyFrame::OnSave));
-    Connect(ID_Load, wxEVT_COMMAND_BUTTON_CLICKED,
-            wxCommandEventHandler(MyFrame::OnLoad));
+    // Event handlers for buttons
+    Connect(ID_Add, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame::OnAdd));
+    Connect(ID_Remove, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame::OnRemove));
+    Connect(ID_Save, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame::OnSave));
+    Connect(ID_Load, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame::OnLoad));
+    Connect(listBox->GetId(), wxEVT_CHECKLISTBOX, wxCommandEventHandler(MyFrame::OnCheck));
+    Connect(searchButton->GetId(), wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame::OnSearch));
 
     Centre();
+    UpdateListBox();
 }
 
 void MyFrame::OnAdd(wxCommandEvent &event) {
@@ -80,10 +92,43 @@ void MyFrame::OnLoad(wxCommandEvent &event) {
     UpdateListBox();
 }
 
+void MyFrame::OnCheck(wxCommandEvent &event) {
+    int index = event.GetInt(); // Get the index of the checked item
+
+    if (index != wxNOT_FOUND) {
+        todoList.rimuoviAttivita(index); // Remove the activity from the TodoList
+
+        listBox->Delete(index); // Remove the item from the wxCheckListBox
+
+        // Update the remaining activities count
+        int remainingTasks = todoList.getAttivita().size();
+        remainingText->SetLabel(wxString::Format("Remaining tasks: %d", remainingTasks));
+    }
+}
+
+void MyFrame::OnSearch(wxCommandEvent &event) {
+    wxString query = searchCtrl->GetValue(); // Use the member variable directly
+
+    if (!query.IsEmpty()) {
+        std::vector<Attivita> risultati = todoList.cercaAttivita(query.ToStdString());
+
+        listBox->Clear();
+        for (const auto &attivita: risultati) {
+            listBox->Append(attivita.getDescrizione());
+        }
+
+        remainingText->SetLabel(wxString::Format("Found tasks: %d", risultati.size()));
+    } else {
+        UpdateListBox(); // Show all activities if the search query is empty
+    }
+}
+
 void MyFrame::UpdateListBox() {
     listBox->Clear();
     std::vector<Attivita> attivita = todoList.getAttivita();
-    for (const auto &attivita: attivita) {
-        listBox->Append(attivita.getDescrizione());
+    for (size_t i = 0; i < attivita.size(); ++i) {
+        listBox->Append(attivita[i].getDescrizione());
+        listBox->Check(i, attivita[i].isCompletato());
     }
+    remainingText->SetLabel(wxString::Format("Remaining activities: %d", todoList.countRemainingActivities()));
 }
